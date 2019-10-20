0f4b80f3551e227ab9e2640050298470bf1efd2e
Nazar Dub <ndub@conceptsoft.io>
2019-10-20 15:54:29 +0300

    Add empty spring boot + gradle application

---
+++ src/main/java/com/njdub/springmongosample/SpringMongoSampleApplication.java
@@ -0,0 +1,13 @@
+package com.njdub.springmongosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njdub/springmongosample/SpringMongoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njdub.springmongosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
